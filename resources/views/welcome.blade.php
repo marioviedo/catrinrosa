6111081cc656d6505c60f2925c7053b100db9696
marioviedo <lmarioviedob@outlook.com>
2021-07-30 23:54:27 -0500

    blur to an image in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,8 +29,8 @@
 
         <!-- information with background image -->
         <div class="bg-main-image-4 h-screen bg-center xl:bg-cover md:bg-cover flex items-center">
-            <div class="max-w-xl">                
-                <p class="text-left text-xl overflow-clip break-words">
+            <div class="max-w-xl ">                
+                <p class="pl-4 text-left text-xl overflow-clip break-words backdrop-filter backdrop-blur-lg">
                     Sencillas, sonrientes y llenas de ingenuidad, como la musulmana Schehrazada, su madre suculenta que las dió a luz en el misterio; fermentando con emoción en los brazos de un príncipe sublime —lúbrico y feroz—, bajo la mirada enternecida de Alah, clemente y misericordioso.
                     Al venir al mundo fueron delicadamente mecidas por las manos de la lustral Doniazada, su buena tía
                 </p>                
